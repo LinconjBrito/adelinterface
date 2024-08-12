9359eae57080ed058a1685f2226f68623d708616
Lincon Brito <linconjbrito@gmail.com>
2024-08-11 23:58:25 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,70 +15,108 @@ class MeuEscalonador extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        resizeToAvoidBottomInset: true,
-        appBar: AppBar(title: Center(child: Image.asset("assets/images/logo.png")), backgroundColor: const Color(0xFF131F24),),
+        resizeToAvoidBottomInset: false, // Impede a mudança do tamanho do layout com o teclado
+        appBar: AppBar(
+          title: Center(child: Image.asset("assets/images/logo.png")),
+          backgroundColor: const Color(0xFF131F24),
+        ),
         backgroundColor: fundo,
-        body: Padding(
-          padding: const EdgeInsets.all(16.0), // Adiciona um padding geral
-          child: ListView(
-
-            children: [
-              Text(
-                'Informe a quantidade de processos:',
-                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) )
-              ),
-              const SizedBox(height: 32.0,),
-
-
-               // Espaçamento entre texto e campo de entrada
-              Row(
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0), // Adiciona um padding geral
+              child: ListView(
                 children: [
-                  NumberInputWidget(),
+                  Text(
+                    'Informe a quantidade de processos:',
+                    style: GoogleFonts.asap(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFFE5E5E5),
+                    ),
+                  ),
+                  const SizedBox(height: 32.0),
+
+                  // Espaçamento entre texto e campo de entrada
+                  Row(
+                    children: [
+                      NumberInputWidget(),
+                    ],
+                  ),
+                  const SizedBox(height: 70.0),
+
+                  Text(
+                    'Informe qual o quantum do processo:',
+                    style: GoogleFonts.asap(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFFE5E5E5),
+                    ),
+                  ),
+                  const SizedBox(height: 32.0),
+
+                  Row(
+                    children: [
+                      NumberInputWidget(),
+                    ],
+                  ),
+                  const SizedBox(height: 70.0),
+
+                  Text(
+                    'Informe a sobrecarga do processo:',
+                    style: GoogleFonts.asap(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFFE5E5E5),
+                    ),
+                  ),
+                  const SizedBox(height: 32.0),
+                  
+                  // Espaçamento entre texto e campo de entrada
+                  Row(
+                    children: [
+                      NumberInputWidget(),
+                    ],
+                  ),
+
+                  const SizedBox(height: 60.0),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      // Ação a ser executada quando o botão é pressionado
+                      print('Botão pressionado!');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red, // Cor de fundo do botão
+                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0), // Padding interno
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0), // Bordas arredondadas
+                      ),
+                    ),
+                    child: Text(
+                      'Escalonar',
+                      style: TextStyle(fontSize: 30.0), // Tamanho do texto
+                    ),
+                  )
+
+
+                  
+
+                  
                 ],
               ),
-
-              const SizedBox(height: 85.0,),
-
-
-
-              Text(
-                'Informe qual o quantum do processo:',
-                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) ),
-              ),
-              const SizedBox(height: 32.0,),
-
-              Row(
-                children: [
-                  NumberInputWidget(),
-                ],
-              ),
-              const SizedBox(height: 85.0,),
-
-              Text(
-                'Informe a sobrecarga do processo:',
-                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) ),
-              ),
-              const SizedBox(height: 32.0,),
- // Espaçamento entre texto e campo de entrada
-              Row(
-                children: [
-                  NumberInputWidget(),
-                ],
-              ),
-
-              Container(
-              child: Image.asset("assets/images/ilustracaop1.png", width: 20,) ,
             ),
-            ],
-
-
-            
-          ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                child: Image.asset("assets/images/ilustracaop1.png", width: 120),
+              ),
+            ),
+          ],
         ),
         
-        bottomNavigationBar: BottomAppBar(
-          color: Color(0xff1C272C),
-          child: Center(child: Text("Geral", style: GoogleFonts.asap(fontSize: 20.0, color: Color(0xffFF2D55), fontWeight: FontWeight.w600),),),),
+        
       ),
     );
   }
