a0d88d4316b1a14980ab15064db998cb936b7996
Lincon Brito <linconjbrito@gmail.com>
2024-08-11 22:48:56 -0300

    Primeira tela criada

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:escalonadors0/widgets/inputs.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final Color fundo = Color(0xFF131f24);
+final Color fundo = Color(0xFF131F24);
 
 void main() {
   runApp(MeuEscalonador());
@@ -14,47 +15,70 @@ class MeuEscalonador extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(title: Center(child: Image.asset("assets/images/logo.png")), backgroundColor: const Color(0xFF131F24),),
         backgroundColor: fundo,
         body: Padding(
           padding: const EdgeInsets.all(16.0), // Adiciona um padding geral
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
+
             children: [
-              const Text(
+              Text(
                 'Informe a quantidade de processos:',
-                style: TextStyle(color: Colors.white, fontSize: 22),
+                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) )
               ),
-              const SizedBox(height: 8), // Espaçamento entre texto e campo de entrada
+              const SizedBox(height: 32.0,),
+
+
+               // Espaçamento entre texto e campo de entrada
               Row(
                 children: [
                   NumberInputWidget(),
                 ],
               ),
-              const SizedBox(height: 16), // Espaçamento entre os grupos de elementos
-              const Text(
+
+              const SizedBox(height: 85.0,),
+
+
+
+              Text(
                 'Informe qual o quantum do processo:',
-                style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) ),
               ),
-              const SizedBox(height: 8), // Espaçamento entre texto e campo de entrada
+              const SizedBox(height: 32.0,),
+
               Row(
                 children: [
                   NumberInputWidget(),
                 ],
               ),
-              const SizedBox(height: 16), // Espaçamento entre os grupos de elementos
-              const Text(
+              const SizedBox(height: 85.0,),
+
+              Text(
                 'Informe a sobrecarga do processo:',
-                style: TextStyle(color: Colors.white, fontSize: 22),
+                style: GoogleFonts.asap(fontSize: 22, fontWeight: FontWeight.w600, color: const Color(0xFFE5E5E5) ),
               ),
-              const SizedBox(height: 8), // Espaçamento entre texto e campo de entrada
+              const SizedBox(height: 32.0,),
+ // Espaçamento entre texto e campo de entrada
               Row(
                 children: [
                   NumberInputWidget(),
                 ],
               ),
+
+              Container(
+              child: Image.asset("assets/images/ilustracaop1.png", width: 20,) ,
+            ),
             ],
+
+
+            
           ),
         ),
+        
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xff1C272C),
+          child: Center(child: Text("Geral", style: GoogleFonts.asap(fontSize: 20.0, color: Color(0xffFF2D55), fontWeight: FontWeight.w600),),),),
       ),
     );
   }
--- lib/widgets/inputs.dart
+++ lib/widgets/inputs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,8 +28,14 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconButton(
-          icon: Icon(Icons.remove),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFF34C759), width: 2.0 ),
+            borderRadius: BorderRadius.circular(50.0)
+          ),
+          child: IconButton(
+            color: Color(0xFF34C759),
+          icon: Icon(Icons.remove,),
           onPressed: () {
             setState(() {
               _currentValue = int.parse(_controller.text);
@@ -37,15 +44,27 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
             });
           },
         ),
+        ),
+
+        SizedBox(width: 22.0,),
+
+        
+        
         SizedBox(
-          width: 80,
+          width: 120,
           child: TextField(
-            style: TextStyle(color: Color(0xff52656d)),
+            style: GoogleFonts.asap(fontSize: 20, color: const Color(0xffe5e5e5) ),
             controller: _controller,
             keyboardType: TextInputType.number,
             textAlign: TextAlign.center,
             decoration: InputDecoration(
-              border: OutlineInputBorder(),
+              contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Color(0xff373D49)),
+                borderRadius: BorderRadius.circular(12.0)
+              ),
+              
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
             ),
             onChanged: (value) {
               setState(() {
@@ -54,15 +73,24 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
             },
           ),
         ),
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              _currentValue = int.parse(_controller.text);
-              _currentValue++;
-              _controller.text = _currentValue.toString();
-            });
-          },
+        
+        SizedBox(width: 22.0,),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFFFF3B30), width: 2.0 ),
+            borderRadius: BorderRadius.circular(50.0)
+          ),
+          child: IconButton(
+            color: Color(0xFFFF3B30),
+            icon: const  Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                _currentValue = int.parse(_controller.text);
+                _currentValue++;
+                _controller.text = _currentValue.toString();
+              });
+            },
+          ),
         ),
       ],
     );
